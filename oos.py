b83e8d02fd63db0e4e779328a405a9e79265707d
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2024-03-14 05:18:47 +0900

    add access control

--- oos.py
+++ oos.py
@@ -8,6 +8,12 @@ from dataclasses import dataclass
 - ポリモーフィズム
 """
 
+class MethodAccessDenied(Exception):
+    pass
+
+class MethodNotFound(Exception):
+    pass
+
 @dataclass
 class Instance:
     class_name: str
@@ -22,16 +28,52 @@ class Class:
     constructor:callable
     methods:dict
 
-class ObjectOrientedSystem:
+class PublicMethod:
+    def __init__(self,method):
+        self.method = method
+
+class PrivateMethod:
+    def __init__(self,method):
+        self.method = method
+
+class PublicAttr:
+    def __init__(self,name):
+        self.name = name
+
+class PrivateAttr:
+    def __init__(self,name):
+        self.name = name
+
+class ReadonlyAttr:
+    def __init__(self,name):
+        self.name = name
+
+class InternalObjectOrientedSystem:
     def __init__(self):
         self.classes = {}
         self.instances = {}
 
     def def_class(self,name,bases,fields,constructor,methods={}):
-        self.classes[name] = Class(name,bases,fields,constructor,methods)
+        _methods = dict(methods)
 
-    def get_instance(self,name):
-        return self.instances[name]
+        #自動でsetter/getter
+        for attr in fields:
+            attr_name = attr.name
+            def getter(sys,this,**args):
+                return this.attributes[attr_name]
+            def setter(sys,this,**args):
+                this.attributes[attr_name] = args["value"]
+
+            GetterMethodClass,SetterMethodClass = {
+                PublicAttr: (PublicMethod,PublicMethod),
+                PrivateAttr: (PrivateMethod,PrivateMethod),
+                ReadonlyAttr: (PublicMethod,PrivateMethod)
+            }[type(attr)]
+
+            _methods[f"get-{attr_name}"] = GetterMethodClass(getter)
+            _methods[f"set-{attr_name}"] = SetterMethodClass(setter)
+
+        self.classes[name] = Class(name,bases,fields,constructor,_methods)
 
     def make_instance(self,class_name,instance_name,**argv):
         _class = self.classes[class_name]
@@ -40,19 +82,35 @@ class ObjectOrientedSystem:
         _class.constructor(self,instance,**argv)
         return instance
 
-    def call(self,obj,method,**argv):
-        instance = self.instances[obj.name]
+    def call_by_name(self,instance_name,method,**argv):
+        instance = self.instances[instance_name]
+        methods = self.classes[instance.class_name].methods
+
+        for name,f in methods.items():
+            if name == method:
+                if isinstance(f,PrivateMethod):
+                    raise MethodAccessDenied(f"{method} is PrivateMethod")
+                elif isinstance(f,PublicMethod):
+                    return self.call(instance,method,**argv)
+
+        raise MethodNotFound(f"{method} not found")
+
+    def call(self,instance,method,**argv):
         methods = self.classes[instance.class_name].methods
         if method in methods:
-            return methods[method](self,instance,**argv)
+            return methods[method].method(self,instance,**argv)
         else:
-            prefix = "set-"
-            if method.startswith(prefix):
-                attr = method[len(prefix):]
-                instance.attributes[attr] = argv["value"]
-                return
-            prefix = "get-"
-            if method.startswith(prefix):
-                attr = method[len(prefix):]
-                return instance.attributes[attr]
-            raise
+            raise MethodNotFound(f"{method} not found")
+
+class ObjectOrientedSystem:
+    def __init__(self):
+        self.sys = InternalObjectOrientedSystem()
+
+    def def_class(self,name,bases,fields,constructor,methods={}):
+        self.sys.def_class(name,bases,fields,constructor,methods)
+
+    def make_instance(self,class_name,instance_name,**argv):
+        return self.sys.make_instance(class_name,instance_name,**argv)
+
+    def call(self,instance_name,method,**argv):
+        return self.sys.call_by_name(instance_name,method,**argv)
--- test_oos.py
+++ test_oos.py
@@ -1,41 +1,87 @@
-from oos import ObjectOrientedSystem
+from oos import *
+import pytest
 
 def test_attr():
     system = ObjectOrientedSystem()
     system.def_class(
         "bank", #クラス名
         tuple(), # 継承
-        ("dollars",), #属性
-        lambda sys,this,**args:  sys.call(this,"set-dollars",value=args["dollars"]), #コンストラクタ
-    )
-    instance = system.make_instance("bank","my-account",dollars=100)
-    assert system.call(instance,"get-dollars") == 100 
-
-def test_auto_attr():
-    system = ObjectOrientedSystem()
-    system.def_class(
-        "bank", #クラス名
-        tuple(), # 継承
-        ("yen",), #属性
+        (PublicAttr("yen"),), #属性
         lambda sys,this,**args:  sys.call(this,"set-yen",value=args["yen"]), #コンストラクタ
     )
-    instance = system.make_instance("bank","my-account",yen=100)
-    assert system.call(instance,"get-yen") == 100 
+    system.make_instance("bank","my-account",yen=100)
+    assert system.call("my-account","get-yen") == 100 
 
-def test_method():
+def test_private_attr():
     system = ObjectOrientedSystem()
     system.def_class(
         "bank", #クラス名
         tuple(), # 継承
-        ("dollars",), #属性
+        (PrivateAttr("dollars"),), #属性
+        lambda sys,this,**args:  sys.call(this,"set-dollars",value=args["dollars"]), #コンストラクタ
+    )
+    system.make_instance("bank","my-account",dollars=100)
+    with pytest.raises(MethodAccessDenied):
+        system.call("my-account","get-dollars")
+    with pytest.raises(MethodAccessDenied):
+        system.call("my-account","set-dollars")
+
+def test_readonly_attr():
+    system = ObjectOrientedSystem()
+    system.def_class(
+        "bank", #クラス名
+        tuple(), # 継承
+        (ReadonlyAttr("dollars"),), #属性
+        lambda sys,this,**args:  sys.call(this,"set-dollars",value=args["dollars"]), #コンストラクタ
+    )
+    system.make_instance("bank","my-account",dollars=100)
+    assert system.call("my-account","get-dollars") == 100 
+    with pytest.raises(MethodAccessDenied):
+        system.call("my-account","set-dollars",value=200)
+
+def test_bank():
+    system = ObjectOrientedSystem()
+    system.def_class(
+        "bank", #クラス名
+        tuple(), # 継承
+        (PublicAttr("dollars"),), #属性
         lambda sys,this,**args:  sys.call(this,"set-dollars",value=args["dollars"]), #コンストラクタ
         methods = {
-            "deposit": lambda sys,this,**args: sys.call(this,"set-dollars",value=sys.call(this,"get-dollars")+args["value"]),
-            "withdraw": lambda sys,this,**args: sys.call(this,"set-dollars",value=max(0,sys.call(this,"get-dollars")-args["value"]))
+            "deposit": PublicMethod(lambda sys,this,**args: sys.call(this,"set-dollars",value=sys.call(this,"get-dollars")+args["value"])),
+            "withdraw": PublicMethod(lambda sys,this,**args: sys.call(this,"set-dollars",value=max(0,sys.call(this,"get-dollars")-args["value"])))
         }
     )
-    instance = system.make_instance("bank","my-account",dollars=100)
-    system.call(instance,"deposit",value=50)
-    assert system.call(instance,"get-dollars") == 150 
-    system.call(instance,"withdraw",value=200)
-    assert system.call(instance,"get-dollars") == 0
+    system.make_instance("bank","my-account",dollars=100)
+    system.call("my-account","deposit",value=50)
+    assert system.call("my-account","get-dollars") == 150 
+    system.call("my-account","withdraw",value=200)
+    assert system.call("my-account","get-dollars") == 0
+
+def test_public_method():
+    system = ObjectOrientedSystem()
+    system.def_class(
+        "bank", #クラス名
+        tuple(), # 継承
+        (PublicAttr("dollars"),), #属性
+        lambda sys,this,**args:  sys.call(this,"set-dollars",value=args["dollars"]), #コンストラクタ
+        methods = {
+            "deposit": PublicMethod(lambda sys,this,**args: sys.call(this,"set-dollars",value=sys.call(this,"get-dollars")+args["value"])),
+        }
+    )
+    system.make_instance("bank","my-account",dollars=100)
+    system.call("my-account","deposit",value=50)
+
+def test_private_method():
+    system = ObjectOrientedSystem()
+    system.def_class(
+        "bank", #クラス名
+        tuple(), # 継承
+        (PublicAttr("dollars"),), #属性
+        lambda sys,this,**args:  sys.call(this,"set-dollars",value=args["dollars"]), #コンストラクタ
+        methods = {
+            "deposit": PrivateMethod(lambda sys,this,**args: sys.call(this,"set-dollars",value=sys.call(this,"get-dollars")+args["value"])),
+        }
+    )
+    system.make_instance("bank","my-account",dollars=100)
+    with pytest.raises(MethodAccessDenied):
+        system.call("my-account","deposit",value=50)
