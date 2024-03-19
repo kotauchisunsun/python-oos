eb870eabe688788de097a1d0be7126afdd0320c3
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2024-03-20 05:28:36 +0900

    fix

--- oos.py
+++ oos.py
@@ -12,25 +12,19 @@ class ObjectOrientedSystem:
     def __init__(self) -> None:
         self.environment = Environment()
 
-        def define(sys: ObjectOrientedSystem, **argv: Any):
-            attr_or_fallback = (
-                lambda name, default: sys.send("args", "get-%s" % name)
-                if sys.send("args", "has", attr=name)
-                else default
-            )
-
+        def define(sys: ObjectOrientedSystem):
             self.environment.define(
                 sys.send("args", "get-name"),
-                attr_or_fallback("bases", []),
-                attr_or_fallback("attrs", []),
-                attr_or_fallback("constructor", lambda sys: None),
-                attr_or_fallback("methods", {}),
+                sys.send("args", "get", attr="bases", fallback=[]),
+                sys.send("args", "get", attr="attrs", fallback=[]),
+                sys.send("args", "get", attr="constructor", fallback=lambda sys: None),
+                sys.send("args", "get", attr="methods", fallback={}),
             )
 
-        def new(sys: ObjectOrientedSystem, **argv: Any):
+        def new(sys: ObjectOrientedSystem):
             cls = sys.send("args", "get-cls")
             name = sys.send("args", "get-name")
-            instance = self.environment.new(cls, name, **argv)
+            instance = self.environment.new(cls, name)
 
             with self.environment:
                 self.environment.register_instance("this", instance)
@@ -52,36 +46,42 @@ class ObjectOrientedSystem:
         instance = self.environment.get_instance(instance_name)
 
         if instance_name == "env":
-            _argv = self.instantiate(argv)
+            _argv = self.instantiate_argv(argv)
             self.environment.register_instance("args", _argv)
 
-            return self.__call(instance, method, **argv)
+            return self.__call(instance, method)
 
         with self.environment:
-            _argv = self.instantiate(argv)
+            _argv = self.instantiate_argv(argv)
             self.environment.register_instance("args", _argv)
 
             self.environment.register_instance("this", instance)
             if instance_name == "this":
-                return self.__call(instance, method, **argv)
+                return self.__call(instance, method)
 
             f = instance.get_method(method)
             if isinstance(f, PrivateMethod):
                 raise MethodAccessDenied(f"{method} is PrivateMethod")
             elif isinstance(f, PublicMethod):
-                return self.__call(instance, method, **argv)
+                return self.__call(instance, method)
 
-    def instantiate(self, argv):
+    def instantiate_argv(self, argv):
         name = "args%f" % random.random()
         self.environment.define(
             name,
             [],
             [PublicAttr(k, v) for k, v in argv.items()],
-            lambda sys, **args: None,
-            {"has": PublicMethod(lambda sys: sys.send("args", "get-attr") in argv)},
+            lambda sys: None,
+            {
+                "get": PublicMethod(
+                    lambda sys: sys.send("this", "get-" + sys.send("args", "get-attr"))
+                    if sys.send("args", "get-attr") in argv
+                    else sys.send("args", "get-fallback")
+                ),
+            },
         )
         return self.environment.new(name, "args")
 
-    def __call(self, instance: Instance, method: str, **argv: MessageType) -> Any:
+    def __call(self, instance: Instance, method: str) -> Any:
         f = instance.get_method(method)
         return f.method(self)
